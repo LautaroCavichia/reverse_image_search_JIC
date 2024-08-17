426babd2675a56bce8d47b2955834aec89c3dd86
LautaroCavichia <lautarocavichia@gmail.com>
2024-08-17 17:42:35 +0200

    first pytorch lighter version.

--- search_utils.py
+++ search_utils.py
@@ -2,43 +2,51 @@ import cv2
 import numpy as np
 import hnswlib
 import os
+import logging
+from efficientnet_pytorch import EfficientNet
+from torchvision import transforms
+from PIL import Image
+import torch
+
+# Impostazioni per usare solo la CPU
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
-from tensorflow.keras.applications import VGG16
-from tensorflow.keras.applications.vgg16 import preprocess_input
-from tensorflow.keras.models import Model
-import tensorflow as tf
-import logging
 
-tf.config.set_visible_devices([], 'GPU')
-
+# Configurazione di logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-base_model = VGG16(weights='imagenet')
-model = Model(inputs=base_model.input, outputs=base_model.get_layer('fc1').output)
+# Carica il modello EfficientNet pre-addestrato
+model_name = 'efficientnet-b0'
+model = EfficientNet.from_pretrained(model_name)
+model.eval()  # Imposta il modello in modalità di valutazione
 
+# Trasformazioni delle immagini
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
 def load_and_preprocess_image(image_path):
-    image = cv2.imread(image_path)
+    image = Image.open(image_path).convert('RGB')
     if image is None:
         logger.error(f"Errore nel caricamento dell'immagine: {image_path}")
         raise ValueError(f"Impossibile caricare l'immagine: {image_path}")
-    image = cv2.resize(image, (224, 224))
-    image = np.expand_dims(image, axis=0)
-    image = preprocess_input(image)
+    image = preprocess(image)
+    image = image.unsqueeze(0)  # Aggiungi una dimensione batch
     return image
-
 
 def extract_features(image_path):
     image = load_and_preprocess_image(image_path)
-    features = model.predict(image)
+    with torch.no_grad():
+        features = model(image)
     # Normalizza le feature
-    features = features.flatten()
+    features = features.numpy().flatten()
     features /= np.linalg.norm(features)
     return features
-
 
 def create_or_load_index(dim, features, index_file):
     hnsw_index = hnswlib.Index(space='l2', dim=dim)
